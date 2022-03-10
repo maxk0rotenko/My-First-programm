f8ebc92fe707535337f2cae5a41489bc8d6dbcf9
Max <max.korotenko.work@gmail.com>
2022-03-10 19:48:42 +0200

    Modify code with Conditions and Cycles

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,7 @@
 "use strict"; 
 
+// #1 console programm practice
+
 const numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDb = {
@@ -11,12 +13,37 @@ const personalMovieDb = {
      
 };
 
-const a = prompt('Один из просмотренных последних фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из просмотренных последних фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из просмотренных последних фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из просмотренных последних фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
+// personalMovieDb.movies[a] = b;
+// personalMovieDb.movies[c] = d;
 
-console.log(personalMovieDb);
+// #2 modify console programm with Сonditions and Сycles
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из просмотренных последних фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDb.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+if (personalMovieDb.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+    console.log('Вы классичсекий зритель');
+} else if (personalMovieDb.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Error');
+}
+
+
+console.log(personalMovieDb);   
