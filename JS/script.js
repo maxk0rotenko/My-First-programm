bf26e33028ce32ab70025fb497419e793ef8cb6d
Max <max.korotenko.work@gmail.com>
2022-03-10 18:09:31 +0200

    My first cod in JS

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+"use strict"; 
+
+const numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDb = {
+    count: numberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+     
+};
+
+const a = prompt('Один из просмотренных последних фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из просмотренных последних фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log(personalMovieDb);
